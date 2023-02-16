f7ad73350de37f94edf8ec8d168859499b883e0d
Tega00 <teezlush@gmail.com>
2023-02-16 07:41:45 -0800

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * code by Tega
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
