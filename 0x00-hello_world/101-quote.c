ff794746f57fac440349df66c07ff4fd2f145d70
Tega00 <teezlush@gmail.com>
2023-02-16 11:35:39 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
