3f28f57b09db4cd7799053d0edddb6319a397afd
Tega00 <teezlush@gmail.com>
2023-02-17 00:42:29 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * Return: 1 (Success)
 */
 int main(void)
+
 {
-write(1, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
